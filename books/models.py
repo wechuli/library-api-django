52e2b735da91b8a8e627c4db7423f0388dcd06be
Wechuli <wechulipaul@yahoo.com>
2018-12-16 17:33:29 +0300

    creating the models and initial routes

---
+++ books/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=250)
+    author = models.CharField(max_length=100)
+    description = models.TextField(max_length=3000)
+    isbn = models.CharField(max_length=13)
+    
+
+    def __str__(self):
+        return self.title
